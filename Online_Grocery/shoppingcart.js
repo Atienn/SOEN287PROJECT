9ed3c73133c25953a41fe7c79089d343fbe9d903
ThisIsQpac <christianjerjian@gmail.com>
2021-03-14 06:41:26 -0400

    Added tax and total in Shopping Cart

--- Online_Grocery/shoppingcart.js
+++ Online_Grocery/shoppingcart.js
@@ -5,30 +5,35 @@ function refreshPage() {
 var itemQuantity;
 window.onload = () => {
   itemQuantity = document.getElementsByClassName("quantity");
-  // listen to something
+
   for (var i = 0; i < itemQuantity.length; i++) {
     itemQuantity[i].onchange = () => {
       updateSubTotal();
     };
   }
   updateSubTotal();
-
   deliveryDate();
 };
 
 function updateSubTotal() {
   var cartItem = document.getElementsByClassName("itemSubTotalPrice");
 
-  var total = 0;
+  var subTotal = 0;
   for (var i = 0; i < cartItem.length; i++) {
     var cartItemSubPrice = cartItem[i].innerHTML;
 
     var quantity = itemQuantity[i].value;
 
     var itemTotal = cartItemSubPrice * quantity;
-    total = total + quantity * cartItemSubPrice;
+    subTotal = subTotal + quantity * cartItemSubPrice;
   }
-  document.getElementById("subTotal").innerHTML = "$" + total.toFixed(2);
+  document.getElementById("subTotal").innerHTML = "$" + subTotal.toFixed(2);
+
+  var tax = subTotal * 0.15;
+  document.getElementById("tax").innerHTML = "$" + tax.toFixed(2);
+
+  var total = subTotal + tax;
+  document.getElementById("total").innerHTML = "$" + total.toFixed(2);
 }
 
 function deliveryDate() {
@@ -37,3 +42,5 @@ function deliveryDate() {
   deliveryDate.setDate(today.getDate() + 2);
   document.getElementById("dateID").innerHTML = deliveryDate.toDateString();
 }
+
+function updateTax() {}
