3e11d5f6b752d3f1a31a1678906ccf59ee6349eb
Atienn <70545238+Atienn@users.noreply.github.com>
2021-03-14 21:38:10 -0400

    Moved Cart Display Function to Appropriate Script

--- Online_Grocery/shoppingcart.js
+++ Online_Grocery/shoppingcart.js
@@ -3,7 +3,8 @@ function refreshPage() {
 }
 
 var itemQuantity;
-window.onload = () => {
+window.addEventListener("load", () => {
+  displayCart();
   itemQuantity = document.getElementsByClassName("quantity");
 
   for (var i = 0; i < itemQuantity.length; i++) {
@@ -13,7 +14,7 @@ window.onload = () => {
   }
   updateSubTotal();
   deliveryDate();
-};
+});
 
 function updateSubTotal() {
   var cartItem = document.getElementsByClassName("itemSubTotalPrice");
@@ -44,3 +45,62 @@ function deliveryDate() {
 }
 
 function updateTax() {}
+
+
+/**
+ * Displays all cart items.
+ */
+function displayCart() {
+  var cartItems = localStorage.getItem('User:Cart');
+  cartItems = JSON.parse(cartItems);
+  var itemsProducts = document.querySelector('.itemsProducts');
+  itemsProducts.innerHTML ='';
+  for(var i=0; i<cartItems.items.length; i++){
+    itemsProducts.innerHTML +=
+    `<div class="row mb-4">
+      <div class="col-md-5 col-lg-3 col-xl-3">
+        <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
+          <a href="#!">
+            <img class="img-fluid w-100" src="${cartItems.items[i].imgSrc}" alt="Sample" />
+          </a>
+        </div>
+      </div>
+      <div class="col-md-7 col-lg-9 col-xl-9">
+        <div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <h5>${cartItems.items[i].name}</h5>
+              <p class="mb-3 text-muted text-uppercase small">
+                IMPORTED FROM: USA
+              </p>
+              <p class="mb-2 text-muted text-uppercase small">
+                Item 2432
+              </p>
+              <p class="mb-3 text-muted text-uppercase small">
+                Weight: 150 g
+              </p>
+            </div>
+            <div>
+              <div class="def-number-input number-input mb-0 w-100">
+                <button onclick="this.parentNode.querySelector('input[type=number]').stepDown(); updateSubTotal();" class="minus">
+                  -
+                </button>
+                <input class="quantity" min="1" name="quantity" value="${cartItems.items[i].amount}" type="number" />
+                <button onclick="this.parentNode.querySelector('input[type=number]').stepUp(); updateSubTotal();" class="plus">
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between align-items-center">
+            <div>
+              <a href="#!" type="button" class="card-link-secondary small text-uppercase mr-3"><i class="fas fa-trash-alt mr-1"></i> Remove item
+              </a>
+            </div>
+            <strong>Price per item: $<strong class="itemSubTotalPrice">${cartItems.items[i].price}</strong></strong>
+          </div>
+        </div>
+      </div>
+    </div>`
+  }
+}
